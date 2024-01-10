09c672ec6534a91f910589c1941bcc33b1d18541
JungleBlame <149385056+JungleBlame@users.noreply.github.com>
2024-01-10 12:11:54 +0000

    Update fixed

--- CodeWars/05_01_23.py
+++ CodeWars/05_01_23.py
@@ -19,8 +19,8 @@ release() - return a string in the format "{MAJOR}.{MINOR}.{PATCH}" """
 class VersionManager:
 
     def __init__ (self,version="0.0.1"):
-
-
+        """Initialise all self variables"""
+    
         self.OG_version=""
         self.version=version
         self.versions_checked=[]
@@ -30,11 +30,11 @@ class VersionManager:
         self.Patch=""
         self.current_ver=""
         self.first=True
-
         self.make_version()
 
     def check_ver(self):
-
+        """Ensures defualt ver and creates ver from input"""
+    
         if self.version=="":
             self.versions_checked=[0,0,1]
         
@@ -63,29 +63,34 @@ class VersionManager:
                         
                     if index==max_index:
                         self.versions_checked.append(listy)
+                        
                     if decimals==3:
                         break
-  
+
         return self
     
     def check_for_enough(self):
-        if len(self.versions_checked) ==1:
+        """Allows for inputs shorted than full ver"""
+    
+        if len(self.versions_checked) >=1:
             self.versions_checked.append("0")
             self.versions_checked.append("0")
+            
         elif len(self.versions_checked) ==2:
             self.versions_checked.append("0")
 
     def make_version(self):
-        
+        """runs checks, applies to returnable variable and ensure initial ver added to history"""
+    
         self.check_ver()
-        self.check_for_enough()
-       
+        self.check_for_enough()    
         self.Major=self.versions_checked[0]
         self.Minor=self.versions_checked[1]
         self.Patch=self.versions_checked[2]
         self.current_ver= "{}.{}.{}".format(self.Major,self.Minor,self.Patch)
 
         if self.first ==True:
+            
             self.history.append(self.current_ver)
             self.OG_version=self.current_ver
 
@@ -93,6 +98,8 @@ class VersionManager:
         return self
 
     def major(self):
+        """Increments major, resets others and adds to history"""
+    
         self.Major=int(self.Major)+1
         self.Minor="0"
         self.Patch="0"
@@ -101,6 +108,8 @@ class VersionManager:
         return self
 
     def minor(self):
+        """Increments minor, resets patch and adds to history"""
+        
         self.Minor=int(self.Minor)+1
         self.Patch="0"
         self.current_ver= "{}.{}.{}".format(self.Major,self.Minor,self.Patch)
@@ -108,34 +117,74 @@ class VersionManager:
         return self
 
     def patch(self):
+        """Increments patch, resets patch and adds to history"""
+        
         self.Patch=int(self.Patch)+1
         self.current_ver= "{}.{}.{}".format(self.Major,self.Minor,self.Patch)
         self.history.append(self.current_ver)
         return self
 
     def rollback(self):
+        """Rollsback though ver history, raises exception if cannot"""
 
         if len(self.history)==1:
-            self.history.append(self.OG_version)
-            raise Exception ("Cannot rollback!")
+                raise Exception ("Cannot rollback!")
+            
         else:
 
             try:    
-                
                     self.history.pop(-1)
                     self.current_ver=self.history[-1]
                     self.version=self.current_ver
                     self.make_version()
             
-            except:
+            except: 
                 raise Exception ("Cannot rollback!")
         
         return self
         
     def release(self):
+        """Returns current ver"""
         
         return self.current_ver
     
-    
+"""
+Top answer:
 
-print(VersionManager().rollback())
+class VersionManager:
+    
+    def __init__(self, version=None):
+        if not version: version='0.0.1'
+        self.__memory = []
+        try:
+            arr = [*map(int,version.split('.')[:3])] + [0,0,0]
+        except:
+            raise Exception("Error occured while parsing version!")
+        del arr[3:]
+        self.__version = arr if any(arr) else [0,0,1]
+    
+    def release(self):
+        return '.'.join(map(str, self.__version))
+
+    def rollback(self):
+        if not self.__memory: raise Exception("Cannot rollback!")
+        self.__version = self.__memory.pop()
+        return self
+        
+    def __update(self, i):
+        self.__memory.append([*self.__version])
+        self.__version[i] += 1
+        self.__version[i+1:] = [0] * (len(self.__version)-1-i)
+        return self
+    
+    def major(self): return self.__update(0)
+    def minor(self): return self.__update(1)
+    def patch(self): return self.__update(2)
+
+
+
+
+
+
+
+"""
